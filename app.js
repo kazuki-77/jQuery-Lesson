88d36102900218945f57dc191bb538dd6d57233c
kauzki-77 <k.sora.7861@gmail.com>
2022-01-03 11:15:37 +0900

    [finish] 5章

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-  $('body').html('<h1> Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').slideDown(function(){; 
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp();
+  });
 });
+/* javascriptでは引数の中に関数を入れることができる */
+/* slideDown()の引数にfunctionを書き加えることで、スライドダウンした後にfunctionを実行することができるようになる */
+/* slideDownの中のfunctionに、cssで色と大きさを変え、slideUp()をすることで用件を満たすことができる */
+/* アニメーションの速度を変えるときはslideDown()の第一引数に数字を入力することで実現する */
+/* 単位はミリ秒なので、１秒の場合は'1000'になる */
